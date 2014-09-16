4a763dc34ece2603c83018608002b0f4613dede6
MK Safi <msafi@msafi.com>
2014-09-16 19:10:12 +0300

    Fix the build.

--- source/config.js
+++ source/config.js
@@ -7,5 +7,4 @@ var heliumConfigurations = {
   "amazonAccountId": "901881000271",
   "amazonCognitoIdentityPoolId": "us-east-1:3b9d95f2-3f63-4d7d-9fd8-75d1462e96db",
   "amazonCognitoRoleArn": "arn:aws:iam::901881000271:role/Cognito_heliumUnauth_DefaultRole",
-  "picture": "http://helium-dev.s3-website-us-east-1.amazonaws.com/content/media/unnamed.jpg"
 };;;
